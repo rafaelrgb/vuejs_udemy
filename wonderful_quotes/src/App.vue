2dfc938d4a60aecfe2536074121d065bb11d6acc
Faerugb <faerugb@gmail.com>
2019-05-01 18:07:55 -0300

    Add a new application for the second course project Wonderful Quotes

---
+++ wonderful_quotes/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <app-header></app-header>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <app-progress-bar></app-progress-bar>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <app-quotes-form></app-quotes-form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <app-quotes></app-quotes>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <app-footer></app-footer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import AppHeader from "./components/shared/Header.vue";
+    import AppProgressBar from "./components/quotes/ProgressBar.vue";
+    import AppQuotesForm from "./components/quotes/QuotesForm.vue";
+    import AppQuotes from "./components/quotes/Quotes.vue";
+    import AppFooter from "./components/shared/Footer.vue";
+
+    export default {
+        components: {
+            'app-header': AppHeader,
+            'app-progress-bar': AppProgressBar,
+            'app-quotes-form': AppQuotesForm,
+            'app-quotes': AppQuotes,
+            'app-header': AppFooter
+        }
+    }
+</script>
+
+<style>
+</style>
