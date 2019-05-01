2360d10d1a3119009cd91049255b7493118ca5aa
Faerugb <faerugb@gmail.com>
2019-05-01 17:46:51 -0300

    Finish aplication for module 9

---
+++ advanced-component-usage/src/components/Red.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row">
+      <div class="col-sm-6">
+        <slot name="title"></slot>
+      </div>
+      <div class="col-sm-6">
+        <slot name="body"></slot>
+      </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid red;
+        background-color: lightcoral;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
